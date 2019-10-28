e21cae33bd3241b19c7fd400f5a01ab95f27db92
gttdo <vinces.gerardo@gmail.com>
2019-10-27 20:09:35 -0700

    fixed port

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express       = require("express");
 var app           = express();
 var bodyParser    = require("body-parser");
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -39,6 +40,6 @@ app.get("/contact", (req, res) =>{
   res.render("contact");
 });
 
-app.listen(3000, function(){
+app.listen(port, function(){
   console.log("Server has started!");
 });
