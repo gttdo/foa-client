2b6a3df5ebbe561d0d00e7448aaed87b908b3fb9
gttdo <vinces.gerardo@gmail.com>
2019-10-30 23:38:09 -0700

    updated service page, add animation

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/views"));
 app.use(express.static(__dirname + "/assets"));
+app.use(express.static(__dirname + "/node_modules"));
 
 app.get("/", function(req, res){
         res.render("index");
