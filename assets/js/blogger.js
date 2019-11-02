8b26ba35ce0dbf345177e24701b8081ac65671bf
gttdo <vinces.gerardo@gmail.com>
2019-11-01 19:41:23 -0700

    update blog page, date, image

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -13,10 +13,18 @@ function myFunction(){
 		type: 'GET',
 
 success: function(data) {
+			var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 			for(var i = 0; i < data.items.length; i++){
-				content.innerHTML += "<h1 class='blogTitle'>" + data.items[i].title + "</h1>";
-				content.innetHTML += "<h3 class='blogPublished'>" + data.items[i].published + "</h3>";
-        content.innerHTML += "<p class='blogContent'>" + data.items[i].content + "</p>";
+				var year = data.items[0].published.substring(0,4);
+				var day = data.items[0].published.substring(8,10);
+				var month = data.items[i].published.substring(5,7);
+				var date = months[month-1] + " " + day + " , " + year;
+
+				content.innerHTML += "<h1 class='blogTitle'>" + data.items[i].title + "</h1>"
+				content.innerHTML += "<h3 class='blogAuthor'>" + data.items[i].author.displayName + "</h3>"
+				content.innerHTML += "<h3 class='blogPublished'>" + date + "</h3>"
+        content.innerHTML += "<p class='blogContent'>" + data.items[i].content + "</p>"
 			}
 		},
 	});
